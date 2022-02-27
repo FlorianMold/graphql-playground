4ec37584ab0dc58071939c841977cd9225f85b31
Florian Mold <florian.mold@agnet.at>
2022-02-27 16:01:56 +0100

    feat: add graphql endpoint to backend

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import {GraphQLModule} from '@nestjs/graphql';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { SetResolver } from './set.resolver';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql']
-    })
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql']      
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SetResolver],
 })
-export class AppModule {}
+export class AppModule { }
